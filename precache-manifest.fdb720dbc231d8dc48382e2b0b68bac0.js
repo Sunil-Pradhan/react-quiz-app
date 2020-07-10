d4474722223db32227ff0a5f46ad2d8b39569d79
Sunil-Pradhan <pradhan.sunil@outlook.com>
2020-07-10 14:28:59 +0530

    Updates

---
+++ precache-manifest.fdb720dbc231d8dc48382e2b0b68bac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacbd56265d041eceb3f11309aa24768",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "913b6c6a159279083193",
+    "url": "/react-quiz-app/static/css/main.a8d7d4ff.chunk.css"
+  },
+  {
+    "revision": "16db850efe01080d09f3",
+    "url": "/react-quiz-app/static/js/2.d5b94d75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.d5b94d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913b6c6a159279083193",
+    "url": "/react-quiz-app/static/js/main.e13c6c8c.chunk.js"
+  },
+  {
+    "revision": "8683e12216164290021d",
+    "url": "/react-quiz-app/static/js/runtime-main.968356ae.js"
+  }
+]);
